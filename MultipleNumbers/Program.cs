aefe3bbc3b08214acf1c7ce7f60ce18f11eb1b10
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-06 20:14:01 +0300

    Add first Task Stepik

---
+++ MultipleNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая считает произведение двух переданных ей чисел.
+// Оператор умножения выглядит так: *
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int mult = a * b;
+
+Console.WriteLine(mult);
