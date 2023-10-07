207b4773bfa6f50b34027f2e20f06792b57ea5af
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-07 21:20:49 +0300

    Add file about coordinate position

---
+++ CoordinatePlanetKona/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Рейнджер: А ты неплохо справился, мы успешно вышли на орбиту. 
+Теперь рассчитай координаты планеты арконцев «Кона».
+Для расчета координат напиши программу, которая выводит сначала сумму, 
+а потом на новой строке частное введённых построчно с консоли дробных чисел.
+*/
+using System;
+public class CoordinatePlanetKona
+{
+    public static void Main() 
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        double sum = x + y;
+        double div = x / y;
+
+        Console.WriteLine(sum);
+        Console.WriteLine(div);
+
+    }
+}
