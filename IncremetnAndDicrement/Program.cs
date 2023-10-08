d0b7f8015e9e193734f3115e3b5f88baa9c73189
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-08 16:40:59 +0300

    Add file about increments and dicrements

---
+++ IncremetnAndDicrement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class IncremetAndDicrement 
+{
+    public static void Main()
+    {
+        /* int a = 1;
+        a = ++a * a;
+        Console.WriteLine(a); */
+
+        /* int a = 1;
+        a = a++ * a;
+        Console.WriteLine(a); */
+
+        /* int a = 1;
+        Console.WriteLine(a++ * a++ * a++);
+        Console.WriteLine(a); */
+
+        int a = 1;
+        a = a * ++a;
+        Console.WriteLine(a);
+    }
+}
