95056c3e9cf3835e87d66d36db1fa3425cc22a54
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-09 12:06:36 +0300

    add file about more number or lower number

---
+++ MoreOrLower/Program.cs
@@ -0,0 +1,25 @@
+﻿/* ГП Авроры: Раз ты так хорошо справляешься, вот еще одно задание. 
+Я отправлю тебе 2 числа, твоя задача вывести на экран результат их сравнения *число 1 результат сравнения число 2*. */
+
+using System;
+public class MoreOrLower
+{
+    public static void Main()
+    {
+        Console.Clear();
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        if (a > b) 
+        {
+            Console.WriteLine(a + " больше " + b);
+        }
+        if (a == b)
+        {
+            Console.WriteLine(a + " равно " + b);
+        }
+        if (a < b) 
+        {
+            Console.WriteLine(a + " меньше " + b);
+        }
+    }
+}
