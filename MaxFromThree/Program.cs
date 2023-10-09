8a9ea34353a8ca25cf6c38fe3da1b7e4f5ad8c7d
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-09 19:18:54 +0300

    Add file Max from three

---
+++ MaxFromThree/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Рейнджер: А теперь небольшая практика, Аврора отправит на твой M.O.V.E 3 числа, 
+каждое с новой строки, выведи наибольшее из этих чисел (гарантируется, что повторы чисел отсутствуют) */
+
+using System;
+public class MaxFromThree
+{
+    public static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        double max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+        Console.WriteLine(max);
+    }
+}
