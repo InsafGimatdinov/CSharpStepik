f012457c24e399f39fecfbd2fa3cd65b8994550d
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-09 20:42:18 +0300

    Add checking file

---
+++ CheckingDivisibility/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Рейнджер: Отлично, может еще задачку? Аврора отправит на твой M.O.V.E четырехзначное число, 
+твоя задача - проверить делится ли первая цифра этого числа на 2 и последняя на 3. 
+Если цифры делятся, выведи True, иначе - False. */
+
+using System;
+public class CheckingDivisibility
+{
+    public static void Main() 
+    {
+        int number = int.Parse(Console.ReadLine());
+        int firstDigit = ((number / 1000) % 10) % 2;
+        int lastDigit = (number % 10) % 3;
+        bool isTrue = true;
+        if (firstDigit == 0 && lastDigit == 0)
+        {
+            Console.WriteLine(isTrue);
+        }
+        else 
+        {
+            Console.WriteLine(!isTrue);
+        }
+    }
+}
