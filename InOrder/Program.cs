ce321b22fd22c07c651be79586779c459a6f3e82
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-10 19:40:33 +0300

    add file about in order numbers

---
+++ InOrder/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Рейнджер: А получится у тебя написать такую программу, которая вначале примет значение, 
+после чего построчно выведет все числа от одного до этого значения (включительно)? */
+
+using System;
+public class InOrder
+{
+    public static void Main() 
+    {
+        int number = int.Parse(Console.ReadLine());
+        int count = 1;
+        while (count <= number)
+        {
+            Console.WriteLine(count);
+            count++;
+        }
+    }
+}
