6e2fd4872942c5730a09e01ee862e6bb86776e02
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-12 19:04:39 +0300

    Add quotes

---
+++ Quotes/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Рейнджер: Я отправлю на твой M.O.V.E строку (какие-то очень древние цитаты),
+ а на следующей строке количество раз, которое нужно ее вывести, твоя задача написать программу, 
+которая выведет заданную строку заданное количество раз. */
+
+using System;
+public class Quotes
+{
+    public static void Main()
+    {
+        string qoute = Console.ReadLine();
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(qoute);
+        }
+    }   
+    
+}
