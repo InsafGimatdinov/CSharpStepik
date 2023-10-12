f0fa8b1ccd26d5624b41e98d36100751e0f07461
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-12 18:58:48 +0300

    Add string C# is the best

---
+++ C#IsTheBest/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Рейнджер: Напиши программу, которая 15 раз выведет "C# is the BEST!". */
+
+using System;
+public class IsTheBest
+{
+    public static void Main()
+    {
+        string text = "C# is the BEST!";
+        for (int i = 0; i <= 15; i++)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
