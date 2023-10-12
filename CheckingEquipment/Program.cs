0c4a45849fc1efc99f2e4809f007d788bd992676
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-12 19:12:59 +0300

    Add checking equipment

---
+++ CheckingEquipment/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Рейнджер: А вот и новая задачка, посмотрим, как ты усвоил тему. Аврора отправит тебе число n - это количество снаряжения, 
+которое мы взяли. Затем n строк, на каждой названия снаряжения. 
+Твоя задача построчно вывести каждый элемент, возможно, удастся найти что-то, что поможет в это ситуации. */
+
+using System;
+public class CheckingEquipment
+{
+    public static void Main()
+    {
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            string equip = Console.ReadLine();
+            Console.WriteLine(equip);
+        }
+    }
+}
