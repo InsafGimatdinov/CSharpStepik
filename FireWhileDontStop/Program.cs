a1660c62e33739d50530eb69c8a2d28268cb98af
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-16 17:24:15 +0300

    Add file about fire

---
+++ FireWhileDontStop/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Рейнджер: Итак, используя цикл for, тебе нужно написать программу, которая будет совершать выстрел "Выстрел совершен", 
+а также писать количество израсходованных боезапасов, при этом, после каждой итерации, 
+программа должна осуществлять возможность остановки огня, как только мы введем "Стоп". */
+
+using System;
+public class FireWhileDontStop
+{
+    public static void Main()
+    {
+        int count = 0;
+        bool condit = true;
+        string empty = Console.ReadLine();
+        for (int i = 0; i < 1000; i++)
+        {
+            empty = Console.ReadLine();
+            Console.WriteLine("Выстрел совершен");
+            count++;
+            Console.WriteLine(count);
+            if (empty == "Стоп")
+            {
+                condit = false;
+                break;
+            }
+        } 
+    }
+}
