e1f1ae7431788cf65c984782777a1ddd262ec81f
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-16 17:30:32 +0300

    Add about while we share

---
+++ WhileWeShare/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Рейнджер: Пока я веду огонь, можешь решить еще одну задачку. Аврора будет отправлять тебе числа, 
+если вдруг число не делится на 7, выведи последнее отправленное число и останови программу. */
+
+using System;
+public class WhileWeShare
+{
+    public static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        for (int i = 0; ; i++)
+        {
+            number = int.Parse(Console.ReadLine());
+            if (number % 7 != 0)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+        }
+    }
+}
